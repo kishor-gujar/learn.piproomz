69aed5b5689b152b68bb278cd9d528f5243ee4c9
Kishor Gujar <kishorgujar95@gmail.com>
2018-04-06 11:17:54 +0530

    firebase and components are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBEFbhrGdKFgO5y02qAgoAqBHc3NUQsxo0',
+    authDomain: 'piproomz-7ed24.firebaseapp.com',
+    databaseURL: 'https://piproomz-7ed24.firebaseio.com',
+    projectId: 'piproomz-7ed24',
+    storageBucket: 'piproomz-7ed24.appspot.com',
+    messagingSenderId: '1098845388678'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
