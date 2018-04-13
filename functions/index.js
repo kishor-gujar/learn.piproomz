bcf960e77ba6d78f0052b69c7757d0b4441d7036
kishor gujar <kishorgujar95@gmail.com>
2018-04-13 14:13:11 +0530

    payment removed

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 
 const admin = require('firebase-admin')
+
 admin.initializeApp(functions.config().firebase);
 
 const stripe = require('stripe')(functions.config().stripe.testkey)
